c7acbce1c9c1799e3572dfa69f63e07fe1686ed9
RutujaSasane <167866463+RutujaSasane@users.noreply.github.com>
2026-02-04 13:24:37 +0530

    Pattern - 4: Right-Angled Number Pyramid - II
    Must do Pattern Problems before starting DSA!

---
+++ 01_Basics/pattern4.py
@@ -0,0 +1,14 @@
+class pattern4:
+    def roots(self, n):
+        for i in range(1, n+1):
+            for j in range(1, i):
+                print(i, end=" ")
+            print()
+
+if __name__ == "__main__":
+    p = pattern4()
+    n = 5
+    p.roots(n)
+
+# Time Complexity: O(N²)
+# Space Complexity: O(1)
